c752d2a7ee1f5daa251f34887ddceb1ffb53a1e5
bruno <brunobalbrasil2@gmail.com>
2023-12-17 11:18:23 -0300

    backend commit

---
+++ src/controllers/MoviesController.js
@@ -0,0 +1,95 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
+class MoviesController {
+  async create(request, response) {
+    const { title, description, rating, tags } = request.body;
+    const { user_id } = request.params;
+
+    const checkRating = rating < 1 || rating > 5;
+
+    if(checkRating) {
+      throw new AppError("A nota deve ser entre 1 e 5.")
+    }
+
+    const [movie_id] = await knex("movies").insert({
+      title,
+      description,
+      rating,
+      user_id
+    });
+
+    const tagsInsert = tags.map(name => {
+      return {
+        movie_id,
+        name,
+        user_id
+      }
+    });
+
+    await knex("tags").insert(tagsInsert);
+
+    response.json();
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const movie = await knex("movies").where({ id }).first();
+    const tags = await knex("tags").where({ movie_id: id }).orderBy("name");
+
+    return response.json({
+      ...movie,
+      tags
+    });
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("movies").where({ id }).delete();
+
+    return response.json();
+  }
+
+  async index(request, response) {
+    const { title, user_id, tags } = request.query;
+
+    let movies;
+
+    if(tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim());
+
+      movies = await knex("tags")
+      .select([
+        "movies.id",
+        "movies.title",
+        "movies.user_id",
+      ])
+      .where("movies.user_id", user_id)
+      .whereLike("movies.title", `%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("movies", "movies.id", "tags.movie_id")
+      .orderBy("movies.title");
+    } else {
+      movies = await knex("movies")
+      .where({ user_id })
+      .whereLike("title", `%${title}`)
+      .orderBy("title");
+    }
+
+    const userTags = await knex("tags").where({ user_id });
+    const moviesWithTags = movies.map(movie => {
+      const movieTags = userTags.filter(tag => tag.movie_id === movie.id);
+
+      return {
+        ...movie,
+        tags: movieTags
+      }
+    });
+
+    return response.json(moviesWithTags);
+  }
+}
+
+module.exports = MoviesController;
